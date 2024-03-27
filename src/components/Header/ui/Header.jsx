edccb58e5ae10b8c87e591658ee068b447f332e0
Danil <dyakovdanilweb@mail.ru>
2024-03-27 13:12:45 +0300

    add ui component - Button, Icon and component Header

---
+++ src/components/Header/ui/Header.jsx
@@ -0,0 +1,35 @@
+import cls from "./Header.module.scss";
+import PizzaIcon from "@/assets/img/logoPizza.svg";
+import ToggleIcon from "@/assets/img/toggleTheme.svg";
+import CartIcon from "@/assets/img/cart.svg";
+import { Button } from "@/ui/button";
+import { Icon } from "@/ui/Icon";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+   const navigate = useNavigate();
+   const onClick = () => {
+      navigate("/");
+   };
+   return (
+      <header className={cls.header}>
+         <div className={cls.container}>
+            <div className={cls.content}>
+               <div className={cls.logo}>
+                  <Icon Svg={PizzaIcon} clickable onClick={onClick} />
+                  <p>WebCademy пицца</p>
+               </div>
+               <div className={cls.buttons}>
+                  <Icon Svg={ToggleIcon} />
+                  <Button border className={cls.button}>
+                     <Icon Svg={CartIcon} />
+                     <span>0 Р</span>
+                  </Button>
+               </div>
+            </div>
+         </div>
+      </header>
+   );
+};
+
+export { Header };
